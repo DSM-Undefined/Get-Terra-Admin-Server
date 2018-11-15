004b047d7816683fa1fd7fc1c0ad948c175626f5
RISMME <python@istruly.sexy>
2018-11-15 22:05:15 +0900

    admin qrcode api

--- Server/view/icon.py
+++ Server/view/icon.py
@@ -1,9 +1,12 @@
+import shutil
 import os
 import zipfile
 import qrcode
+import qrcode.image.svg
 # pillow 설치 필요
 
 from flasgger import swag_from
+from flask import send_file
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -17,6 +20,21 @@ class Icon(Resource):
     @jwt_required
     @swag_from(ICON_GET)
     def get(self):
-        game_ = AdminUserModel.objects(userId=get_jwt_identity()).first()['game']
-        var = [qrcode.make(booth.boothName) for booth in BoothModel.objects(game=game_)]
-        return var
+        factory = qrcode.image.svg.SvgImage
+        game_ = AdminUserModel.objects(userId=get_jwt_identity()).first().game
+
+        directory = 'static/qr/' + str(game_.gameKey)
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+        os.makedirs(directory)
+        directory += '\\'
+
+        zip = zipfile.ZipFile(directory + 'qr.zip', 'w')
+        for booth in BoothModel.objects(game=game_):
+            file_name = directory+booth.boothName+'.svg'
+            qrcode.make(booth.boothName, image_factory=factory).save(file_name)
+
+            zip.write(file_name, booth.boothName+'.svg', compress_type=zipfile.ZIP_DEFLATED)
+        zip.close()
+
+        send_file(directory + 'qr.zip', mimetype='application/zip')
